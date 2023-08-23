cd01e8e2d85b983406f91fbf4974a7108d1eb33d
abolikov <abolikov@artenecy.com>
2023-08-23 16:10:01 +0600

    Дополнил комментарии для более детального изучения

--- basic/syntax/main/basic_types.go
+++ basic/syntax/main/basic_types.go
@@ -6,9 +6,15 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/cmplx"
 )
 
+/*
+uint - безнаковое число (только положительное)
+int - число со знаком (могу быть отрицательные) - int8,16,32,64 - количество занимаемы бит
+float - числа c плавающей точкой
+*/
 var (
 	ToBe   bool       = false
 	MaxInt uint64     = 1<<64 - 1
@@ -19,4 +25,12 @@ func main() {
 	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
 	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
 	fmt.Printf("Type: %T Value: %v\n", z, z)
+	cast()
+}
+
+func cast() {
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
 }
--- basic/syntax/main/main.go
+++ basic/syntax/main/main.go
@@ -55,3 +55,27 @@ func split(sum int) (x, y int) {
 	y = sum - x
 	return
 }
+
+//Константы
+
+const (
+	// Create a huge number by shifting a 1 bit left 100 places.
+	// In other words, the binary number that is 1 followed by 100 zeroes.
+	Big = 1 << 100
+	// Shift it right again 99 places, so we end up with 1<<1, or 2.
+	Small = Big >> 99
+)
+
+func example() {
+	fmt.Printf("v is of type %T\n", needFloat(Big))
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+	//fmt.Println(needInt(Big)) -> выдаст ошибку, так как инт не держит такой объем
+}
+
+func needInt(x int) int { return x*10 + 1 }
+
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
